5ae05a8501b2af9a71120202d2d7bf7c8ea762a5
chuluq <choirulchuluq10@gmail.com>
2022-07-21 08:07:19 +0700

    add icon

--- src/components/RepoItem/index.js
+++ src/components/RepoItem/index.js
@@ -3,11 +3,14 @@ import "./styles.css";
 const RepoItem = ({ repo }) => {
   return (
     <div className="card">
-      <h1 className="card-name">{repo.name}</h1>
       <div className="card-details">
-        <p className="card-id">ID: {repo.id}</p>
-        <p className="card-lang">Language: {repo.lang ?? "-"}</p>
+        <h1 className="card-name">{repo.name}</h1>
+        <div>
+          <p className="card-id">ID: {repo.id}</p>
+          <p className="card-lang">Language: {repo.lang ?? "-"}</p>
+        </div>
       </div>
+      <span className="material-symbols-outlined">info</span>
     </div>
   );
 };
